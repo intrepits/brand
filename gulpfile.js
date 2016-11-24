b0cdcbf4335b895672a664637e0b49b4e528b95a
Andrés Smerkin <andres.smerkin@gmail.com>
2016-11-24 10:14:02 -0300

    Sarting with the Brand styles

---
+++ gulpfile.js
@@ -0,0 +1,49 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var nunjucks = require('gulp-nunjucks');
+var browserSync = require('browser-sync').create(); 
+
+/**
+ *
+ * Styles Compilation
+ *
+ */
+gulp.task('sass', () => {
+	return gulp.src('src/scss/intrepits.scss')
+		.pipe(sass())
+		.pipe(gulp.dest('dist/css'))
+        .pipe(browserSync.stream());
+})
+
+/**
+ *
+ * Images Compilation, minification and improvements
+ *
+ */
+gulp.task('img', () => {
+	return gulp.src('src/img/**/*')
+		.pipe(gulp.dest('dist/img'));
+})
+
+
+/**
+ *
+ * Html generation and compilation 
+ *
+ */
+gulp.task('html', () => {
+	return gulp.src('src/html/index.html')
+		.pipe(nunjucks.compile())
+		.pipe(gulp.dest('dist'))
+        .pipe(browserSync.stream());
+});
+
+
+gulp.task('serve', ['sass', 'html'], () => {
+	browserSync.init({
+		server: { baseDir: './dist/' }
+	});
+
+	gulp.watch('src/scss/**/*.scss', ['sass']);
+	gulp.watch('src/html/**/*.html', ['html']);
+}); 
